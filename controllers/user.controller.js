759015be644b2b18a9bce3ce0492e0067f016625
barbatoz0220 <47182649+barbatoz0220@users.noreply.github.com>
2021-05-13 17:24:41 +0700

    Working on delivering precautions matching the resulting diseases - Modified query for symptom search - Wrote a similar query to get precautions for each matched diseases - Need to rethink the design in some way because it's not yet efficient...

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,29 +1,51 @@
 const db = require('../models/model.database.js');
 
+
 exports.searchSymptom = (req, res) => {
+    // Taking in input symptoms
     const inputSymptoms = req.body.symptoms.split(",").map((value) => {
         return value.trim();
     });
+    var symptomCount = inputSymptoms.length;
+
+    // Input symptom processing to get variable Condition
     var firstSymptom = inputSymptoms.shift();
     var condition = `("${firstSymptom}"`;
     for (symptom of inputSymptoms) {
         condition += `, "${symptom}"`
     }
     condition += `)`;
-    console.log(condition);
-    db.query(`SELECT DISTINCT d.diseaseID, d.diseaseName, d.diseaseDesc, s.symptomName
-            FROM disease d, symptom_disease sd, symptoms s
-            WHERE d.diseaseID = sd.diseaseID AND sd.symptomID = s.symptomID 
-            AND s.symptomName IN ${condition}`, (error, results) => {
 
+    // Start querying for disease with most matches with the condition
+    db.query(`SELECT d.diseaseID, d.diseaseName, d.diseaseDesc
+            FROM disease d
+            INNER JOIN symptom_disease sd
+            ON d.diseaseID = sd.diseaseID
+            INNER JOIN symptoms s
+            ON sd.symptomID = s.symptomID
+            WHERE s.symptomName IN ${condition}
+            GROUP BY d.diseaseID
+            HAVING COUNT(DISTINCT s.symptomName) >= ${symptomCount};`, (error, diseaseResults) => {
         if (error) {
             return res.render("./user/userSearch", {
                 errorMessage: "Error when looking for diseases with your input. Try again!"
             })
         } else {
-            console.log(results);
+            var diseaseArray = Object.values(JSON.parse(JSON.stringify(diseaseResults)));
+            for (disease of diseaseArray) {
+                db.query(`
+                SELECT p.precautionName
+                FROM precaution p
+                INNER JOIN disease_precaution dp
+                ON p.precautionID = dp.precautionID
+                INNER JOIN disease d
+                ON d.diseaseID = dp.diseaseID
+                WHERE d.diseaseName LIKE "${disease.diseaseName}";`, (error, precautionResults) => {
+                    
+                });
+            };
             return res.render("./user/userSearch", {
-                disease: results
+                disease: diseaseResults
             })
         }
     })
